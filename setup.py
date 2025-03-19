ef600b4b35282c138fab43a34ef252d81477c5b4
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-19 15:51:06 +0000

    Test a cutdown setup.py

--- setup.py
+++ setup.py
@@ -5,26 +5,36 @@ version = '0.0.1'
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
+# setup(
+#     name='netbox-certificates',
+#     version=version,
+#     description='A plugin to help manage SSL Certificates',
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     license='GPL 3.0',
+#     author='Seb Harrington',
+#     author_email='seb@cdal.co.uk',
+#     url='https://github.com/NetworkSeb/netbox-certificates',
+#     download_url='https://github.com/NetworkSeb/netbox-certificates/archive/v{}.zip'.format(version),
+#     python_requires='>3.9',
+#     classifiers=[
+#         'Environment :: Plugins',
+#         'Environment :: Web Environment',
+#         'Framework :: Django',
+#         'License :: OSI Approved :: GPL 3.0',
+#         'Topic :: System :: Networking',
+#         'Topic :: System :: Systems Administration'
+#     ],
+#     packages=find_packages(),
+#     include_package_data=True,
+#     zip_safe=False,
+# )
+
 setup(
     name='netbox-certificates',
-    version=version,
+    version='0.0.1',
     description='A plugin to help manage SSL Certificates',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='GPL 3.0',
-    author='Seb Harrington',
-    author_email='seb@cdal.co.uk',
-    url='https://github.com/NetworkSeb/netbox-certificates',
-    download_url='https://github.com/NetworkSeb/netbox-certificates/archive/v{}.zip'.format(version),
-    python_requires='>3.9',
-    classifiers=[
-        'Environment :: Plugins',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'License :: OSI Approved :: GPL 3.0',
-        'Topic :: System :: Networking',
-        'Topic :: System :: Systems Administration'
-    ],
+    install_requires=[],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
