e030eaad81fa19b6834d03850d4459f7e0601d22
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-19 16:23:52 +0000

    Version 0.0.2

--- netbox_certificates/__init__.py
+++ netbox_certificates/__init__.py
@@ -4,7 +4,7 @@ class NetBoxCertificatesConfig(PluginConfig):
     name = 'netbox_certificates'
     verbose_name = 'NetBox Certificates'
     description = 'Model and Manage Certificates in Netbox'
-    version = '0.0.1'
+    version = '0.0.2'
     base_url = 'certificates'
 
 config = NetBoxCertificatesConfig
