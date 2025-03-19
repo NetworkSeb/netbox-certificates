07b52d5a81ab47d68a1401daf2b46e029d7899da
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-19 21:25:20 +0000

    Another one...

--- netbox_certificates/models/certificate.py
+++ netbox_certificates/models/certificate.py
@@ -3,9 +3,6 @@ from netbox.models import NetBoxModel
 from utilities.choices import ChoiceSet
 from django.contrib.postgres.fields import ArrayField
 from django.urls import reverse
-from .certificate_authority import CertificateAuthority
-from .certificate_instance import CertificateInstance
-
 
 # Choices - extendable by key in configuration
 class CertificateStatusChoices(ChoiceSet):
