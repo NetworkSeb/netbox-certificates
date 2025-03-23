936f4741b864803e8f0c4a0429dded514c5a3aa2
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-23 09:58:09 +0000

    Add navigation buttons!

--- netbox_certificates/navigation.py
+++ netbox_certificates/navigation.py
@@ -1,16 +1,50 @@
-from extras.plugins import PluginMenuItem
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+# Buttons
+
+certificate_buttons = [
+    PluginMenuButton(
+        link='plugins:netbox_access_lists:certificate_add',
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+        color=ButtonColorChoices.GREEN
+    )
+]
+
+certificate_authority_buttons = [
+    PluginMenuButton(
+        link='plugins:netbox_access_lists:certificate_authority_add',
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+        color=ButtonColorChoices.GREEN
+    )
+]
+
+certificate_instance_buttons = [
+    PluginMenuButton(
+        link='plugins:netbox_access_lists:certificate_instance_add',
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+        color=ButtonColorChoices.GREEN
+    )
+]
+
 
 menu_items = (
     PluginMenuItem(
         link='plugins:netbox_certificates:certificate_list',
-        link_text='Certificates'
+        link_text='Certificates',
+        buttons=certificate_buttons
     ),
     PluginMenuItem(
         link='plugins:netbox_certificates:certificate_authority_list',
-        link_text='Certificate Authorities'
+        link_text='Certificate Authorities',
+        buttons=certificate_authority_buttons
     ),
     PluginMenuItem(
         link='plugins:netbox_certificates:certificate_instance_list',
-        link_text='Certificate Instances'
+        link_text='Certificate Instances',
+        buttons=certificate_instance_buttons
     ),
 )
