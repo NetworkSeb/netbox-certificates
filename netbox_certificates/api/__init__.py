f1432289edc2b75871e4c5cf6b929a63c9ddd07f
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-24 20:52:39 +0000

    Fix: try again

--- netbox_certificates/api/__init__.py
+++ netbox_certificates/api/__init__.py
@@ -2,4 +2,4 @@ from netbox_certificates.api.serializers_.certificate import *
 from netbox_certificates.api.serializers_.certificate_instance import *
 from netbox_certificates.api.serializers_.certificate_authority import *
 
-from .serializers import *
+from .serializers_ import *
