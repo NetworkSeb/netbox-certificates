16e66482bc2360c174c5369a567cf387e167d074
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-24 20:50:23 +0000

    api Serailizer not initialising independently

--- netbox_certificates/api/__init__.py
+++ netbox_certificates/api/__init__.py
@@ -1,3 +1,5 @@
 from netbox_certificates.api.serializers_.certificate import *
 from netbox_certificates.api.serializers_.certificate_instance import *
 from netbox_certificates.api.serializers_.certificate_authority import *
+
+from .serializers import *
