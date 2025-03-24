7f352a9b9d47023f1db06ada8bb15cae177b944a
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-24 15:57:38 +0000

    Fix: attempt to fix circular import

--- netbox_certificates/api/urls.py
+++ netbox_certificates/api/urls.py
@@ -1,12 +1,12 @@
 from netbox.api.routers import NetBoxRouter
 
-from netbox_certificates.api.views import *
+from netbox_certificates.api.views import CertificateViewSet, CertificateInstanceViewSet, CertificateAuthorityViewSet 
 
 app_name = 'netbox_certificates'
 
 router = NetBoxRouter()
 router.register('certificates', CertificateViewSet)
 router.register('certificateinstance', CertificateInstanceViewSet)
-router.register('certificateauthority', CertificateAuthority)
+router.register('certificateauthority', CertificateAuthorityViewSet)
 
 urlpatterns = router.urls
