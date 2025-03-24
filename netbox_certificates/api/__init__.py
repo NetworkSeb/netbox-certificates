1b255381ab700087bae3b54c5a6d08665a87d010
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-24 12:49:41 +0000

    Fix: attempt to fix circular import of serializers

--- netbox_certificates/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from netbox_certificates.api.serializers_.certificate import *
-from netbox_certificates.api.serializers_.certificate_instance import *
-from netbox_certificates.api.serializers_.certificate_authority import *
