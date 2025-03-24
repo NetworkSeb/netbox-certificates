56136d07b404b6ab3511e75f6dc524a1ab9a9e37
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-24 16:45:57 +0000

    Fix: Testing

--- netbox_certificates/api/serializers.py
+++ netbox_certificates/api/serializers.py
@@ -1,3 +1,146 @@
-# from netbox_certificates.api.serializers_.certificate import *
-# from netbox_certificates.api.serializers_.certificate_instance import *
-# from netbox_certificates.api.serializers_.certificate_authority import *
+from rest_framework import serializers
+
+from netbox.api.serializers import NetBoxModelSerializer, WritableNestedSerializer
+from dcim.api.serializers import NestedDeviceSerializer
+
+from netbox_certificates.models import Certificate
+
+class CertificateSerializer(NetBoxModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificate-detail'
+    )
+
+    instance_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Certificate
+        ca = NestedCertificateAuthoritySerializer()
+        device = NestedDeviceSerializer()
+        instance = NestedCertificateInstanceSerializer()
+
+        fields = (
+            'id',
+            'display',
+            'custom_fields',
+            'cn',
+            'instance_count',
+            'url',
+            'san',
+            'device',
+            'status', 
+            'type', 
+            'install_type', 
+            'fs_cert_location', 
+            'fs_key_location', 
+            'vault_url',
+            'service_commands',
+            'service_check',
+            'service_lb',
+            'ca',
+            'content',
+            'created',
+            'last_updated',
+            'comments',
+            'tags'
+        )
+
+class NestedCertificateSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificate-detail'
+    )
+
+    class Meta:
+        model = Certificate
+        fields = (
+            'id',
+            'cn',
+            'san'
+        )
+
+class CertificateInstanceSerializer(NetBoxModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
+    )
+
+    class Meta:
+        model = CertificateInstance
+        certificate = NestedCertificateSerializer()
+        ca = NestedCertificateAuthoritySerializer()
+
+        fields = (
+            'id',
+            'display',
+            'custom_fields',
+            'url',
+            'created',
+            'last_updated',
+            'pk',
+            'certificate',
+            'ca',
+            'ca_reference',
+            'serial_number',
+            'issue_date',
+            'expiry_date',
+            'status'
+            'csr',
+            'key',
+            'pem',
+            'comments',
+            'tags'
+        )
+
+class NestedCertificateInstanceSerializer(WritableNestedSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
+    )
+
+    class Meta:
+        model = CertificateInstance
+        fields = (
+            'id',
+            'certificate',
+            'ca'
+        )
+
+class CertificateAuthoritySerializer(NetBoxModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificateauthority-detail'
+    )
+
+    certificate_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = CertificateAuthority
+
+        fields = (
+            'id',
+            'display',
+            'custom_fields',
+            'url',
+            'created',
+            'certificate_count',
+            'last_updated',
+            'name',
+            'acme_url',
+            'admin_url',
+            'status',
+            'actions'
+            'comments',
+            'tags'
+        )
+
+class NestedCertificateAuthoritySerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_certificates-api:certificateauthority-detail'
+    )
+
+    class Meta:
+        model = CertificateAuthority
+        fields = (
+            'id',
+            'name'
+        )        
--- netbox_certificates/api/serializers_/certificate_instance.py
+++ netbox_certificates/api/serializers_/certificate_instance.py
@@ -1,52 +1,52 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 
-from netbox.api.serializers import NetBoxModelSerializer, WritableNestedSerializer
+# from netbox.api.serializers import NetBoxModelSerializer, WritableNestedSerializer
 
-from netbox_certificates.models import CertificateInstance
-from netbox_certificates.api.serializers_ import NestedCertificateSerializer, NestedCertificateAuthoritySerializer
+# from netbox_certificates.models import CertificateInstance
+# from netbox_certificates.api.serializers_ import NestedCertificateSerializer, NestedCertificateAuthoritySerializer
 
-class CertificateInstanceSerializer(NetBoxModelSerializer):
+# class CertificateInstanceSerializer(NetBoxModelSerializer):
 
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
-    )
+#     url = serializers.HyperlinkedIdentityField(
+#         view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
+#     )
 
-    class Meta:
-        model = CertificateInstance
-        certificate = NestedCertificateSerializer()
-        ca = NestedCertificateAuthoritySerializer()
+#     class Meta:
+#         model = CertificateInstance
+#         certificate = NestedCertificateSerializer()
+#         ca = NestedCertificateAuthoritySerializer()
 
-        fields = (
-            'id',
-            'display',
-            'custom_fields',
-            'url',
-            'created',
-            'last_updated',
-            'pk',
-            'certificate',
-            'ca',
-            'ca_reference',
-            'serial_number',
-            'issue_date',
-            'expiry_date',
-            'status'
-            'csr',
-            'key',
-            'pem',
-            'comments',
-            'tags'
-        )
+#         fields = (
+#             'id',
+#             'display',
+#             'custom_fields',
+#             'url',
+#             'created',
+#             'last_updated',
+#             'pk',
+#             'certificate',
+#             'ca',
+#             'ca_reference',
+#             'serial_number',
+#             'issue_date',
+#             'expiry_date',
+#             'status'
+#             'csr',
+#             'key',
+#             'pem',
+#             'comments',
+#             'tags'
+#         )
 
-class NestedCertificateInstanceSerializer(WritableNestedSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
-    )
+# class NestedCertificateInstanceSerializer(WritableNestedSerializer):
+#     url = serializers.HyperlinkedIdentityField(
+#         view_name='plugins-api:netbox_certificates-api:certificateinstance-detail'
+#     )
 
-    class Meta:
-        model = CertificateInstance
-        fields = (
-            'id',
-            'certificate',
-            'ca'
-        )
+#     class Meta:
+#         model = CertificateInstance
+#         fields = (
+#             'id',
+#             'certificate',
+#             'ca'
+#         )
