650801cfed6d96de105fb4ae946e8e12c8c8f9f2
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-25 21:11:28 +0000

    More naming changes

--- netbox_certificates/models/certificate.py
+++ netbox_certificates/models/certificate.py
@@ -68,6 +68,7 @@ class Certificate(NetBoxModel):
     device = models.ManyToManyField(
         to='dcim.Device',
         blank=True,
+        allow_null=True
         verbose_name='Device',
         help_text='Device(s) with certificate installed'
     )
