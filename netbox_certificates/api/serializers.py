a2d4ac43fae0e12dae7eab05654dbd14e62ac14a
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-25 19:26:35 +0000

    Missing .

--- netbox_certificates/api/serializers.py
+++ netbox_certificates/api/serializers.py
@@ -2,4 +2,4 @@ from .serializers_.certificate import *
 from .serializers_.certificate_instance import *
 from .serializers_.certificate_authority import *
 
-from nested_serializers import *
+from .nested_serializers import *
