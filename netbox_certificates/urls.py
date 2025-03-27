c78ea71c3b15f7cc1a3a538f9ce1eb1fb86233b2
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-03-27 11:16:52 +0000

    Attempt using register model views

--- netbox_certificates/urls.py
+++ netbox_certificates/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from netbox.views.generic import ObjectChangeLogView
+from utilities.urls import get_model_urls
 
 from netbox_certificates.views import CertificateListView, CertificateEditView, CertificateView, CertificateDeleteView, CertificateInstanceListView, CertificateInstanceEditView, CertificateInstanceView, CertificateInstanceDeleteView, CertificateAuthorityListView, CertificateAuthorityEditView, CertificateAuthorityView, CertificateAuthorityDeleteView
 from netbox_certificates.models import Certificate, CertificateInstance, CertificateAuthority
@@ -13,6 +14,10 @@ urlpatterns = (
     path('certificates/<int:pk>/edit/', CertificateEditView.as_view(), name='certificate_edit'),
     path('certificates/<int:pk>/delete/', CertificateDeleteView.as_view(), name='certificate_delete'),
     path('certificates/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='certificate_changelog', kwargs={'model': Certificate}),
+
+    path(
+        "certificates/", include(get_model_urls("netbox_certificates", "certificates"))
+    ),
 
     # Certificate Instances
     path('certificateinstance/', CertificateInstanceListView.as_view(), name='certificateinstance_list'),
--- netbox_certificates/views/certificate.py
+++ netbox_certificates/views/certificate.py
@@ -1,5 +1,6 @@
 from netbox.views import generic
 from django.db.models import Count
+from utilities.views import register_model_view
 
 from netbox_certificates.models import Certificate
 from netbox_certificates.forms import CertificateForm, CertificateFilterForm
@@ -31,3 +32,10 @@ class CertificateEditView(generic.ObjectEditView):
 
 class CertificateDeleteView(generic.ObjectDeleteView):
     queryset = Certificate.objects.all()
+
+@register_model_view(Certificate, "bulk_import", detail=False)
+class CertificateBulkImportView(generic.BulkImportView):
+    queryset = Certificate.objets.all()
+    model_form = CertificateBulkImportView
+    table = CertificateTable
+    default_return_url = "plugins:netbox_certificates:certificate_list"
