1f6628e6d53c0ec922206d4774d5acc802c2b924
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-08-03 22:00:48 +0100

    Add initial attempt to get journaling and contact assignment working

--- netbox_certificates/__init__.py
+++ netbox_certificates/__init__.py
@@ -2,9 +2,12 @@ from netbox.plugins import PluginConfig
 
 class NetBoxCertificatesConfig(PluginConfig):
     name = 'netbox_certificates'
-    verbose_name = 'NetBox Certificates'
     description = 'Model and Manage Certificates in Netbox'
     version = '0.0.2'
+    author = 'Network Seb'
     base_url = 'certificates'
-
-config = NetBoxCertificatesConfig
+    verbose_name = 'Netbox Certificates'
+    default_settings = {}
+    required_settings = []
+    min_version = '4.2.0'
+    features = ['journaling', 'contact_assignments']
