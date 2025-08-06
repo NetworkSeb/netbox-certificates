892e954c3bfc52c75bd3e82f4570f2114c999d1f
Seb Harrington <s.m.harrington@sussex.ac.uk>
2025-08-06 23:43:42 +0100

    Refactor CertificateInstanceCalendarView to use TemplateResponseMixin and set content type for iCal feed

--- netbox_certificates/views/certificate_instance.py
+++ netbox_certificates/views/certificate_instance.py
@@ -1,5 +1,5 @@
 from netbox.views import generic
-from django.views.generic import View
+from django.views.generic.base import TemplateResponseMixin
 from django.shortcuts import render
 from django.db.models import Count
 from utilities.views import register_model_view
@@ -61,11 +61,12 @@ class CertificateInstanceBulkDeleteView(generic.BulkDeleteView):
     table = CertificateInstanceTable
 
 @register_model_view(CertificateInstance, name="calendar", detail=False)
-class CertificateInstanceCalendarView(View):
+class CertificateInstanceCalendarView(TemplateResponseMixin):
     """
     Render all certificate instances as an iCal feed
     """
     def get(self, request):
+        content_type='text/calendar'
         certificates = CertificateInstance.objects.order_by('expiry_date').all()
         template_name = "netbox_certificates/certificates.ics"
 
